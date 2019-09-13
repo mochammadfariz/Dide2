1ebedf87df30af2219aadb1f0f582702b6bb88c7
mochammadfariz <mochammadfariz11@gmail.com>
2019-09-13 18:58:18 +0700

    adding button and toast

--- app/src/main/java/com/fariz/dadu/MainActivity.kt
+++ app/src/main/java/com/fariz/dadu/MainActivity.kt
@@ -2,11 +2,19 @@ package com.fariz.dadu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        val kaku tidak bisa diubah nilainya
+            val rollButton : Button = findViewById(R.id.roll_button)
+            rollButton.setOnClickListener {
+                Toast.makeText(this,"Role Dice !!!",Toast.LENGTH_SHORT).show()
+            }
     }
 }
