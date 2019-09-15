1a657ff023da283c986379f767726cb602436eea
mochammadfariz <mochammadfariz11@gmail.com>
2019-09-15 22:17:15 +0700

    adding private fun and random method

--- app/src/main/java/com/fariz/dadu/MainActivity.kt
+++ app/src/main/java/com/fariz/dadu/MainActivity.kt
@@ -3,7 +3,9 @@ package com.fariz.dadu
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
+import android.widget.ImageView
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,7 +16,20 @@ class MainActivity : AppCompatActivity() {
 //        val kaku tidak bisa diubah nilainya
             val rollButton : Button = findViewById(R.id.roll_button)
             rollButton.setOnClickListener {
-                Toast.makeText(this,"Role Dice !!!",Toast.LENGTH_SHORT).show()
+                rollDice()
             }
+    }
+
+    private fun rollDice() {
+        val diceImage : ImageView = findViewById(R.id.image_dice)
+        val drawableRes = when(Random().nextInt(6)+1){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableRes)
     }
 }
