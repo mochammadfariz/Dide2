5b04288e66622420d834fb8782a3bcd72e2b9bb2
mochammadfariz <mochammadfariz11@gmail.com>
2019-09-16 21:34:45 +0700

    using databinding

--- app/src/main/java/com/fariz/dadu/MainActivity.kt
+++ app/src/main/java/com/fariz/dadu/MainActivity.kt
@@ -2,26 +2,28 @@ package com.fariz.dadu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.ImageView
+import androidx.databinding.DataBindingUtil
+import com.fariz.dadu.databinding.ActivityMainBinding
 import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
 
+//    lateinit var rollButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val binding : ActivityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
         setContentView(R.layout.activity_main)
 
-//        val kaku tidak bisa diubah nilainya
-            val rollButton : Button = findViewById(R.id.roll_button)
-            rollButton.setOnClickListener {
+//            rollButton  = findViewById(R.id.roll_button)
+            binding.rollButton.setOnClickListener {
                 rollDice()
             }
     }
 
     private fun rollDice() {
-        val diceImage : ImageView = findViewById(R.id.image_dice)
+   val diceImage : ImageView = findViewById(R.id.image_dice)
         val drawableRes = when(Random().nextInt(6)+1){
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
